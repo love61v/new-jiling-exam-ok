5c25968ca95a65bcd5c97369e06f78a2ccbbaef7
Administrator <Administrator@hgx-PC>
2015-07-07 22:07:31 +0800

    kj

--- exam-jinling/src/main/java/com/happy/exam/controller/examtype/ExamTypeAction.java
+++ exam-jinling/src/main/java/com/happy/exam/controller/examtype/ExamTypeAction.java
@@ -26,23 +26,23 @@ import com.happy.exam.model.SystemUser;
 import com.happy.exam.service.ExamTypeService;
 
 /**
- *  部门action
- *
+ * 考试类型action
+ * 
  * @version : Ver 1.0
- * @author	: <a href="mailto:h358911056@qq.com">hubo</a>
- * @date	: 2015年6月15日 下午10:45:36 
+ * @author : <a href="mailto:h358911056@qq.com">hubo</a>
+ * @date : 2015年6月15日 下午10:45:36
  */
 @Controller
 @RequestMapping("/examtype")
-public class ExamTypeAction extends BaseAction{
+public class ExamTypeAction extends BaseAction {
 
 	@Autowired
 	private ExamTypeService examTypeService;
+
 	/**
-	 * 模块表表页面
-	 *
-	 * @author 	: <a href="mailto:hubo@95190.com">hubo</a>  
-	 * 2015年5月16日 下午11:48:57
+	 * 试题类型表表页面
+	 * 
+	 * @author : <a href="mailto:954643309@qq.com">hgx</a> 2015-7-7 下午10:05:38
 	 * @param model
 	 * @param request
 	 * @return
@@ -51,12 +51,12 @@ public class ExamTypeAction extends BaseAction{
 	public String showExamTypes(Model model) {
 		return "examtype/list";
 	}
-	
+
 	/**
-	 * 模块树页面
-	 *
-	 * @author 	: <a href="mailto:hubo@95190.com">hubo</a>  
-	 * 2015年5月16日 下午11:48:57
+	 * 试题类型树页面
+	 * 
+	 * @author : <a href="mailto:954643309@qq.com">hgx</a> 2015-7-7 下午10:05:38
+	 *         2015年5月16日 下午11:48:57
 	 * @param model
 	 * @param request
 	 * @return
@@ -65,104 +65,105 @@ public class ExamTypeAction extends BaseAction{
 	public String choiceExamTypeTree(Model model) {
 		return "examtype/choiceExamTypeTree";
 	}
-	
-	
+
 	/**
-	 * 返回treeGrid模块数据
-	 *
-	 * @author 	: <a href="mailto:hubo@95190.com">hubo</a>  
+	 * 返回treeGrid试题类型数据
+	 * 
+	 * @author : <a href="mailto:954643309@qq.com">hgx</a> 2015-7-7 下午10:05:38
 	 * @date 2015年5月16日 下午11:56:08
 	 * @param request
 	 * @return
 	 */
 	@RequestMapping(value = "/list.json", method = RequestMethod.POST)
 	@ResponseBody
-	public TreegridDto showExamTypelist(ExamType module) {
+	public TreegridDto showExamTypelist(ExamType examType) {
 		TreegridDto treegridDto = new TreegridDto();
-		 
-		List<ExamType> list = examTypeService.findTreegrid(module);
+
+		List<ExamType> list = examTypeService.findTreegrid(examType);
 		treegridDto.setRows(list);
 		treegridDto.setTotal(list.size());
 		return treegridDto;
 	}
-	
+
 	/**
 	 * 跳转到编辑用户页面
-	 *
-	 * @author 	: <a href="mailto:h358911056@qq.com">hubo</a>  2015年6月7日 下午5:11:03
+	 * 
+	 * @author : <a href="mailto:954643309@qq.com">hgx</a> 2015-7-7 下午10:05:38
 	 * @param model
 	 * @param request
 	 * @return
-	 * @throws UnsupportedEncodingException 
+	 * @throws UnsupportedEncodingException
 	 */
 	@RequestMapping(value = "/beforeEditExamType.html", method = RequestMethod.GET)
-	public String beforeEditExamType(Model model, String id,String pname,String parentId,String flag) throws UnsupportedEncodingException {
-		if(StringUtils.isNotBlank(flag) && flag.equals("2")){//修改用户信息回选
-			ExamType module = examTypeService.getById(Long.valueOf(id), ExamType.class);
-			model.addAttribute("module", module);
+	public String beforeEditExamType(Model model, String id, String pname,
+			String parentId, String flag) throws UnsupportedEncodingException {
+		if (StringUtils.isNotBlank(flag) && flag.equals("2")) {// 修改用户信息回选
+			ExamType examType = examTypeService.getById(Long.valueOf(id),
+					ExamType.class);
+			model.addAttribute("examType", examType);
 		}
 		model.addAttribute("flag", flag);
 		model.addAttribute("pid", parentId);
-		if(StringUtils.isNotBlank(pname)){
-			model.addAttribute("pname", java.net.URLDecoder.decode(pname,"UTF-8"));
+		if (StringUtils.isNotBlank(pname)) {
+			model.addAttribute("pname",
+					java.net.URLDecoder.decode(pname, "UTF-8"));
 		}
-		
+
 		return "examtype/edit";
 	}
-	
+
 	/**
-	 * 编辑用户
-	 * 存在ID则修改，否则添加
-	 *
-	 * @author 	: <a href="mailto:h358911056@qq.com">hubo</a>  2015年6月7日 下午11:45:43
+	 * 编辑用户 存在ID则修改，否则添加
+	 * 
+	 * @author : <a href="mailto:954643309@qq.com">hgx</a> 2015-7-7 下午10:05:38
 	 * @param model
-	 * @param module
+	 * @param examType
 	 * @return
 	 */
 	@RequestMapping(value = "/editExamtype.json", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> editExamType(Model model, ExamType module) {
+	public Map<String, Object> editExamType(Model model, ExamType examType) {
 		Map<String, Object> map = getStatusMap();
-		
+
 		int count = 0;
-		if(null != module && null!=module.getId()){//修改用户
-			module.setUpdateTime(new Date());
-			SystemUser systemUser=getCurrentSystemUser();
-			module.setCreateUser(systemUser.getCreateUser());
-			count = examTypeService.update(module);
-			map.put("flag", module.getId());
-		}else{//添加
-			module.setStatus(1);
-			//module.setId();
-			SystemUser systemUser=getCurrentSystemUser();
-			module.setCreateUser(systemUser.getCreateUser());
-			module.setCreateTime(new Date());
-			count =  examTypeService.save(module);
+		if (null != examType && null != examType.getId()) {// 修改用户
+			examType.setUpdateTime(new Date());
+			SystemUser systemUser = getCurrentSystemUser();
+			examType.setCreateUser(systemUser.getCreateUser());
+			count = examTypeService.update(examType);
+			map.put("flag", examType.getId());
+		} else {// 添加
+			examType.setStatus(1);
+			// examType.setId();
+			SystemUser systemUser = getCurrentSystemUser();
+			examType.setCreateUser(systemUser.getCreateUser());
+			examType.setCreateTime(new Date());
+			count = examTypeService.save(examType);
 			map.put("flag", null);
 		}
 		map.put("status", count);
-		
+
 		return map;
 	}
-	
+
 	/**
-	 * 删除模块
-	 *
-	 * @author 	: <a href="mailto:h358911056@qq.com">hubo</a>  2015年6月7日 下午3:30:41
-	 * @param id  当前选中节点的id
-	 * @param parnetId  不为空则上节点为父节点，则删除自身与其下所有子节点
+	 * 删除试题类型
+	 * 
+	 * @author : <a href="mailto:954643309@qq.com">hgx</a> 2015-7-7 下午10:05:38
+	 * @param examType
 	 * @return
 	 */
-	@RequestMapping(value="/deleteExamtype.json",method=RequestMethod.POST)
-	@ResponseBody public Map<String, Object> deleteExamType(ExamType module){
+	@RequestMapping(value = "/deleteExamtype.json", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> deleteExamType(ExamType examType) {
 		Map<String, Object> map = getStatusMap();
-		
-		if(null!=module.getId()){
-			int count = examTypeService.deleteUnion(module);
+
+		if (null != examType.getId()) {
+			int count = examTypeService.deleteUnion(examType);
 			map.put("status", count);
 		}
-		
+
 		return map;
 	}
-	
+
 }
