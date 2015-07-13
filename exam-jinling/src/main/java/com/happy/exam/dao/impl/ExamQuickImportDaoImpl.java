79b37fc6955675c2849d6c369bc619fc2a112fc2
hgx <hgx@qq.com>
2015-07-13 23:34:35 +0800

    sdf

---
+++ exam-jinling/src/main/java/com/happy/exam/dao/impl/ExamQuickImportDaoImpl.java
@@ -0,0 +1,16 @@
+package com.happy.exam.dao.impl;  
+import com.happy.exam.dao.ExamQuickImportDao; 
+import com.happy.exam.model.ExamQuickImport;
+import org.springframework.stereotype.Service;
+
+/**
+ *  ExamQuickImportDao
+ *
+ * @version : Ver 1.0
+ * @author	: <a href="mailto:hubo@95190.com">hubo</a>
+ * @date	: 2015年5月17日 下午9:01:26 
+ */
+@Service
+public class ExamQuickImportDaoImpl extends MybatisBaseDaoImpl<ExamQuickImport, java.lang.Long> implements ExamQuickImportDao { 
+
+}
