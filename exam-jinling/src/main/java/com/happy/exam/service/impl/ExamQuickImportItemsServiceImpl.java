424d81acd4e0ec1c6e7a5d6e11ec6e43f8fdf23f
hgx <hgx@qq.com>
2015-07-13 23:35:05 +0800

    sdfsdsf

---
+++ exam-jinling/src/main/java/com/happy/exam/service/impl/ExamQuickImportItemsServiceImpl.java
@@ -0,0 +1,17 @@
+package com.happy.exam.service.impl;  
+import com.happy.exam.service.ExamQuickImportItemsService;
+import com.happy.exam.service.impl.MybatisBaseServiceImpl;
+import com.happy.exam.model.ExamQuickImportItems;
+import org.springframework.stereotype.Service;
+
+/**
+ *  ExamQuickImportItemsServiceImpl
+ *
+ * @version : Ver 1.0
+ * @author	: <a href="mailto:hubo@95190.com">hubo</a>
+ * @date	: 2015年5月17日 下午9:01:26 
+ */
+@Service
+public class ExamQuickImportItemsServiceImpl extends MybatisBaseServiceImpl<ExamQuickImportItems, java.lang.Long> implements ExamQuickImportItemsService { 
+
+}
---
+++ exam-jinling/src/main/java/com/happy/exam/service/impl/ExamQuickImportServiceImpl.java
@@ -0,0 +1,17 @@
+package com.happy.exam.service.impl;  
+import com.happy.exam.service.ExamQuickImportService;
+import com.happy.exam.service.impl.MybatisBaseServiceImpl;
+import com.happy.exam.model.ExamQuickImport;
+import org.springframework.stereotype.Service;
+
+/**
+ *  ExamQuickImportServiceImpl
+ *
+ * @version : Ver 1.0
+ * @author	: <a href="mailto:hubo@95190.com">hubo</a>
+ * @date	: 2015年5月17日 下午9:01:26 
+ */
+@Service
+public class ExamQuickImportServiceImpl extends MybatisBaseServiceImpl<ExamQuickImport, java.lang.Long> implements ExamQuickImportService { 
+
+}
